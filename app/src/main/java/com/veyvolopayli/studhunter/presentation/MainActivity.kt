4acb7ba332c5ab904dc44426bac154107a101139
veyvolopayli <60238433+veyvolopayli@users.noreply.github.com>
2023-05-11 01:08:30 +0300

    some

--- app/src/main/java/com/veyvolopayli/studhunter/presentation/MainActivity.kt
+++ app/src/main/java/com/veyvolopayli/studhunter/presentation/MainActivity.kt
@@ -5,27 +5,24 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.veyvolopayli.studhunter.presentation.home_screen.HomeViewModel
 import com.veyvolopayli.studhunter.R
+import com.veyvolopayli.studhunter.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val vm: HomeViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
+//    private val navController: NavController by lazy { findNavController(R.id.nav_graph) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val intent = intent
-        val token = intent.getStringExtra("token")
-
-//        Toast.makeText(this, token, Toast.LENGTH_SHORT).show()
-
-        vm.state.observe(this) {
-            Log.e("Publications", it.publications.toString())
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 
--- app/src/main/java/com/veyvolopayli/studhunter/presentation/start_screen/StartFragment.kt
+++ app/src/main/java/com/veyvolopayli/studhunter/presentation/start_screen/StartFragment.kt
@@ -5,16 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.veyvolopayli.studhunter.R
+import com.veyvolopayli.studhunter.databinding.FragmentStartBinding
+import com.veyvolopayli.studhunter.presentation.sign_in_screen.SignInFragment
+import com.veyvolopayli.studhunter.presentation.sign_up_screen.SignUpFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class StartFragment : Fragment() {
+
+    private lateinit var binding: FragmentStartBinding
+    private val signInFragment = SignInFragment()
+    private val signUpFragment = SignUpFragment()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false)
+    ): View {
+
+        binding = FragmentStartBinding.inflate(layoutInflater, container, false)
+
+        val navController = Navigation.findNavController(binding.root)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(1000)
+            navController.navigate(R.id.action_start_to_sign_in_fragment)
+        }
+
+        return binding.root
     }
 
 }
